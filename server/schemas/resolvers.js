e34e600be28c631f1795374055d708aa6fb10a76
Jared Tichacek <jared.tichacek@gmail.com>
2023-05-08 11:51:38 -0500

    added resolvers

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,75 @@
+const { User } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+        // get a single user by either their id or their username
+        me: async (parent, args, context) => {
+            if (context.user) {
+                return User.findOne({ _id: context.user_id });
+            }
+            throw new AuthenticationError('You need to be logged in!');
+        },
+    },
+
+    Mutation: {
+        // create a user, sign a token, and send it back (to client/src/components/SignUpForm.js)
+        addUser: async (parent, { username, email, password }) => {
+            const user = await User.create({ username, email, password });
+            const token = signToken(user);
+
+            return { token, user };
+        },
+        // login a user, sign a token, and send it back (to client/src/components/LoginForm.js)
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+
+            if (!user) {
+                throw new AuthenticationError('No user found');
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+
+            const token = signToken(user);
+
+            return { token, user };
+        },
+        // save a book to a user's `savedBooks` field by adding it to the set (to prevent duplicates)
+        saveBook: async (parent, { bookData }, context) => {
+            if (context.user) {
+                return User.findOneAndUpdate(
+                    { _id: context.user_id },
+                    {
+                        $addToSet: {
+                            savedBooks: bookData,
+                        }
+                    },
+                    {
+                        new: true,
+                        runValidators: true,
+                    }
+                )
+            }
+            // if user isn't logged in, throw an error
+            throw new AuthenticationError('You need to be logged in!');
+        },
+        removeBook: async (parent, { bookId }, context) => {
+            if (context.user) {
+                return User.findOneAndUpdate(
+                    { _id: context.user_id },
+                    { $pull: { savedBooks: { bookId } } },
+                    { new: true }
+                    )
+                }
+                // if user isn't logged in, throw an error
+                throw new AuthenticationError('You need to be logged in!');
+            },
+        },
+    };
+
+    module.exports = resolvers;
